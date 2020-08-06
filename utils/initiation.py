bab7cf77daf4e454bb70d39961d02b2e002d20bc
Nick Machnik <nick.machnik@gmail.com>
2020-08-06 18:30:55 +0200

    Draft Uniprot, Pfam, GO tables and their relations

--- utils/initiation.py
+++ utils/initiation.py
@@ -33,3 +33,46 @@ def _create_table(connection, create_table_sql):
         c.execute(create_table_sql)
     except Error as e:
         print(e)
+
+
+def initiate_db(path):
+    sql_create_Pfam = """CREATE TABLE IF NOT EXISTS Pfam (  # noqa: F841
+                        accession text PRIMARY KEY NOT NULL,
+                        id text NOT NULL UNIQUE,
+                    );"""
+
+    sql_create_GO = """CREATE TABLE IF NOT EXISTS GO (
+                        id text PRIMARY KEY NOT NULL,
+                        name text NOT NULL UNIQUE,
+                    );"""
+
+    sql_create_PfamGORelation = """CREATE TABLE IF NOT EXISTS PfamGORelation(
+                                    Pfam_accession text NOT NULL,
+                                    GO_id text NOT NULL,
+                                    FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
+                                    FOREIGN KEY (GO_id) REFERENCES GO(id),
+                                    UNIQUE (Pfam_accession, GO_id)
+                                );"""
+
+    sql_create_uniprot = """CREATE TABLE IF NOT EXISTS UniProt (
+                            accession text PRIMARY KEY,
+                            entry_name text NOT NULL UNIQUE,
+                        );"""
+
+    sql_create_PfamUniProtRelation = """CREATE TABLE IF NOT EXISTS PfamUniProtRelation(
+                                    Pfam_accession text NOT NULL,
+                                    UniProt_accession text NOT NULL,
+                                    position text NOT NULL,
+                                    FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
+                                    FOREIGN KEY (UniProt_accession) REFERENCES UniProt(accession),
+                                    UNIQUE (Pfam_accession, GO_id)
+                                );"""
+
+    # create db
+    connection = _connect(path)
+
+    # create tables
+    if connection is not None:
+        pass
+    else:
+        print("Error! cannot create the database connection.")
