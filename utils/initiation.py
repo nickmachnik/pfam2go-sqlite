52a3925c4324239e2332bd322f19c08c1962e6f8
Nick Machnik <nick.machnik@gmail.com>
2020-08-07 15:54:13 +0200

    Fix bug in sql create table statement

--- utils/initiation.py
+++ utils/initiation.py
@@ -36,43 +36,44 @@ def _create_table(connection, create_table_sql):
 
 
 def initiate_db(path):
-    sql_create_Pfam = """CREATE TABLE IF NOT EXISTS Pfam (  # noqa: F841
+    tables = []
+    tables.append("""CREATE TABLE IF NOT EXISTS Pfam (
                         accession text PRIMARY KEY NOT NULL,
-                        id text NOT NULL UNIQUE,
-                    );"""
+                        id text NOT NULL UNIQUE);""")
 
-    sql_create_GO = """CREATE TABLE IF NOT EXISTS GO (
+    tables.append("""CREATE TABLE IF NOT EXISTS GO (
                         id text PRIMARY KEY NOT NULL,
-                        name text NOT NULL UNIQUE,
-                    );"""
+                        name text NOT NULL UNIQUE);""")
 
-    sql_create_PfamGORelation = """CREATE TABLE IF NOT EXISTS PfamGORelation(
-                                    Pfam_accession text NOT NULL,
-                                    GO_id text NOT NULL,
-                                    FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
-                                    FOREIGN KEY (GO_id) REFERENCES GO(id),
-                                    UNIQUE (Pfam_accession, GO_id)
-                                );"""
+    # tables.append("""CREATE TABLE IF NOT EXISTS PfamGORelation(
+    #                                 Pfam_accession text NOT NULL,
+    #                                 GO_id text NOT NULL,
+    #                                 FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
+    #                                 FOREIGN KEY (GO_id) REFERENCES GO(id),
+    #                                 UNIQUE (Pfam_accession, GO_id)
+    #                             );""")
 
-    sql_create_uniprot = """CREATE TABLE IF NOT EXISTS UniProt (
-                            accession text PRIMARY KEY,
-                            entry_name text NOT NULL UNIQUE,
-                        );"""
+    # tables.append("""CREATE TABLE IF NOT EXISTS UniProt (
+    #                         accession text PRIMARY KEY,
+    #                         entry_name text NOT NULL UNIQUE,
+    #                     );""")
 
-    sql_create_PfamUniProtRelation = """CREATE TABLE IF NOT EXISTS PfamUniProtRelation(
-                                    Pfam_accession text NOT NULL,
-                                    UniProt_accession text NOT NULL,
-                                    position text NOT NULL,
-                                    FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
-                                    FOREIGN KEY (UniProt_accession) REFERENCES UniProt(accession),
-                                    UNIQUE (Pfam_accession, GO_id)
-                                );"""
+    # tables.append("""CREATE TABLE IF NOT EXISTS PfamUniProtRelation(
+    #                                 Pfam_accession text NOT NULL,
+    #                                 UniProt_accession text NOT NULL,
+    #                                 position text NOT NULL,
+    #                                 FOREIGN KEY (Pfam_accession) REFERENCES Pfam(accession),
+    #                                 FOREIGN KEY (UniProt_accession) REFERENCES UniProt(accession),
+    #                                 UNIQUE (Pfam_accession, GO_id)
+    #                             );""")
 
     # create db
     connection = _connect(path)
 
     # create tables
     if connection is not None:
-        pass
+        with connection:
+            for create_table_sql in tables:
+                _create_table(connection, create_table_sql)
     else:
         print("Error! cannot create the database connection.")
