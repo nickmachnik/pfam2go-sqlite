3840ef69f13d1ba445bbfd35bf486101c0549c77
Nick Machnik <nick.machnik@gmail.com>
2020-08-10 15:18:04 +0200

    Make common db, test different init steps

--- tests/test_initiation.py
+++ tests/test_initiation.py
@@ -8,14 +8,17 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestInitiation(unittest.TestCase):
-    def test_init(self):
+    def __init__(self, *args, **kwargs):
+        super(TestInitiation, self).__init__(*args, **kwargs)
         pfam2go_test_path = os.path.join(
             THIS_DIR, 'test_data/pfam2go_small.txt')
-        test_db_name = "test.db"
-        if os.path.isfile(test_db_name):
-            os.remove(test_db_name)
-        initiation.initiate_db(test_db_name, pfam2go_test_path)
-        connection = initiation._connect(test_db_name)
+        self.test_db_name = "test.db"
+        if os.path.isfile(self.test_db_name):
+            os.remove(self.test_db_name)
+        initiation.initiate_db(self.test_db_name, pfam2go_test_path)
+
+    def test_init_tables(self):
+        connection = initiation._connect(self.test_db_name)
         with connection:
             c = connection.cursor()
             c.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -24,6 +27,16 @@ class TestInitiation(unittest.TestCase):
             db_descr,
             [('Pfam',), ('GO',), ('PfamGORelation',),
              ('UniProt',), ('PfamUniProtRelation',)])
+
+    def test_init_insert_pfam(self):
+        connection = initiation._connect(self.test_db_name)
+        with connection:
+            c = connection.cursor()
+            c.execute("SELECT * FROM Pfam")
+            pfam_table = c.fetchall()
+        self.assertEqual(
+            pfam_table,
+            [('PF00001', '7tm_1'), ('PF00002', '7tm_2'), ('PF00003', '7tm_3')])
 
 
 if __name__ == '__main__':
