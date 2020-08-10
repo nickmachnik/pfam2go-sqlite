0be5deef687a9c91bcf150932faf4e9127d01202
Nick Machnik <nick.machnik@gmail.com>
2020-08-10 15:01:46 +0200

    Add pfam2go test data for db init

--- tests/test_initiation.py
+++ tests/test_initiation.py
@@ -4,13 +4,17 @@ import unittest
 from utils import initiation
 import os
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class TestInitiation(unittest.TestCase):
     def test_init(self):
+        pfam2go_test_path = os.path.join(
+            THIS_DIR, 'test_data/pfam2go_small.txt')
         test_db_name = "test.db"
         if os.path.isfile(test_db_name):
             os.remove(test_db_name)
-        initiation.initiate_db(test_db_name)
+        initiation.initiate_db(test_db_name, pfam2go_test_path)
         connection = initiation._connect(test_db_name)
         with connection:
             c = connection.cursor()
